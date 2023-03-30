10d7d58ccf24e8c9e1b4451af4364d392897bb93
Quang <bnnquang@gmail.com>
2023-03-30 22:17:20 +0700

    first commit

---
+++ src/hello.rs
@@ -0,0 +1,14 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use cortex_m_rt::entry;
+use cortex_m_semihosting::hprintln;
+
+#[entry]
+fn main() -> ! {
+    hprintln!("Hello, world!").unwrap();
+
+    loop {}
+}
